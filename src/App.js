e88917679f221f7b286de272b20049672645657f
robert vanarsdall <Rob@roberts-Mac.local>
2020-07-18 01:15:07 -0400

    app fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import ParentComponent from "./PassingProps/ParentComponent";
 
 function App() {
   return (
     <div style={{ textAlign: "center" }}>
       <p>Testing the App.js file</p>
-      <ParentComponent />
     </div>
   );
 }
